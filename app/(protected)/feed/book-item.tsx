dd46ef6bc334fa5172f49f5ae2bb943e828f16e0
Chitolina <lucas.chitolina01@gmail.com>
2024-06-10 23:38:25 -0300

    chore: Add drawer component to book item click for better user experience

--- app/(protected)/feed/book-item.tsx
+++ app/(protected)/feed/book-item.tsx
@@ -1,17 +1,31 @@
 import { Book } from "@/db/schema";
 import { Card } from "@/components/ui/card";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import Link from "next/link";
 
 export const BookItem = (book: Book) => {
   return (
-    <Link href={`/books/${book.id}`}>
-      <Card
-        key={book.id}
-        className="p-4 shadow-md hover:bg-slate-400 duration-300 flex justify-between items-center h-64"
-      >
+    <Drawer direction="right">
+      <DrawerTrigger asChild>
+        <Card
+          key={book.id}
+          className="p-4 shadow-md hover:bg-slate-400 duration-300 flex justify-between items-center h-64"
+        >
+          <h2 className="font-bold">{book.name}</h2>
+          <p>{book.author}</p>
+        </Card>
+      </DrawerTrigger>
+      <DrawerContent className="w-96 flex flex-col p-4 gap-2">
         <h2 className="font-bold">{book.name}</h2>
         <p>{book.author}</p>
-      </Card>
-    </Link>
+
+        <Link
+          className="p-4 bg-emerald-500 rounded-md"
+          href={`/books/${book.id}`}
+        >
+          View
+        </Link>
+      </DrawerContent>
+    </Drawer>
   );
 };
