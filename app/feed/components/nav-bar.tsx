0977e5f8307c610f292fb93b065b5fdafb98befa
manulongh <emanuellibarboza@gmail.com>
2024-06-10 22:02:47 -0300

    navbar layout

---
+++ app/feed/components/nav-bar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import { Settings, User, HomeIcon } from "lucide-react"
+
+interface ItemProps {
+    icon: ReactNode
+    href: string
+    children: ReactNode
+    active?: boolean
+}
+
+function NavBarItem(props: ItemProps) {
+    return (
+        <Link className="hover:bg-slate-100 duration-200 py-2 px-3 rounded-md flex gap-2 items-center" href={props.href}>
+            <span className={`p-1 bg-gray-400 rounded-lg ${props.active && "bg-emerald-500"}`}>{props.icon}</span>
+            {props.children}
+        </Link>
+    )
+}
+
+export default function NavBar() {
+    return (
+        <nav className="flex-1 h-full bg-slate-50 max-w-44 p-4 flex flex-col gap-4 font-medium">
+            <NavBarItem href="/teste" active icon={<Settings className="text-white" size={16}  />}>
+                Texto
+            </NavBarItem>
+            <NavBarItem href="/teste" icon={<User className="text-white" size={16} />}>
+                Texto
+            </NavBarItem>
+
+        </nav>
+    );
+}
--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -1,18 +1,12 @@
 import AuthButton from "@/components/AuthButton";
+import NavBar from "./components/nav-bar";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-screen h-screen flex flex-row">
-      <nav className="flex-1 max-w-44 bg-gray-300 p-4">
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-      </nav>
-      <div className="flex flex-col w-full">
+      <NavBar />
+      <div className="flex-1">
         <header className="flex justify-between items-center p-4 bg-gray-600">
           <h1>Read Realm</h1>
           <AuthButton />
