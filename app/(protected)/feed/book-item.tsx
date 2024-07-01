c796f2445a1a3b001b05b45391861661a58faf04
Chitolina <lucas.chitolina01@gmail.com>
2024-07-01 19:01:30 -0300

    fixing link to book

--- app/(protected)/feed/book-item.tsx
+++ app/(protected)/feed/book-item.tsx
@@ -7,38 +7,21 @@ import { Button } from "@/components/ui/button";
 
 export const BookItem = (book: Book) => {
   return (
-    <Drawer direction="right">
-      <DrawerTrigger asChild>
-        <div
-          key={book.id}
-          className="p-4 bg-transparent border-none flex flex-col items-center gap-2"
-        >
-          <Image
-            alt={book.title + " cover"}
-            src={book.cover}
-            width={150}
-            height={250}
-            className="object-cover aspect-[8/12]"
-          />
-          <h2 className="font-bold">{book.title}</h2>
-        </div>
-      </DrawerTrigger>
-      <DrawerContent className="w-96 flex flex-col p-4 gap-2 items-center justify-center">
-        <h2 className="font-bold text-2xl">{book.title}</h2>
-
+    <Link href={`/books/${book.id}/edit`}>
+      Ver livro
+      <div
+        key={book.id}
+        className="p-4 bg-transparent border-none flex flex-col items-center gap-2"
+      >
         <Image
           alt={book.title + " cover"}
           src={book.cover}
-          width={250}
-          height={350}
+          width={150}
+          height={250}
+          className="object-cover aspect-[8/12]"
         />
-
-        <Button asChild>
-          <Link className="p-4 mt-4 rounded-md" href={`/books/${book.id}`}>
-            Ver livro
-          </Link>
-        </Button>
-      </DrawerContent>
-    </Drawer>
+        <h2 className="font-bold">{book.title}</h2>
+      </div>
+    </Link>
   );
 };
